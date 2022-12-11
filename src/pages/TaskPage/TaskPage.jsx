b182d4cbe4292a8fd212b47a6c449e714243fda9
3str <ssrghome@gmail.com>
2022-12-11 16:08:38 +0300

    added showing answer

--- src/pages/TaskPage/TaskPage.jsx
+++ src/pages/TaskPage/TaskPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useParams } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Accordion from "../../components/UI/Accordion/Accordion";
